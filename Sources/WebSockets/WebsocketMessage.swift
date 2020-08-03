76d41ddfcf71b5194bad8c78332c64114d287c83
Kirill Danilov <kyrylo.danylov@dstarlab.com>
2020-08-03 15:31:15 +0300

    add websocketmessage

---
+++ Sources/WebSockets/WebsocketMessage.swift
@@ -0,0 +1,12 @@
+import Vapor
+
+struct WebsocketMessage<T: Codable>: Codable {
+    let client: UUID
+    let data: T
+}
+
+extension ByteBuffer {
+    func decodeWebsocketMessage<T: Codable>(_ type: T.Type) -> WebsocketMessage<T>? {
+        try? JSONDecoder().decode(WebsocketMessage<T>.self, from: self)
+    }
+}
